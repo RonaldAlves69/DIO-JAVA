5ae0a36295e5a658e40b81849a28ebbf2d13bc16
RonaldAlves69 <ronaldqwe2015@gmail.com>
2024-10-05 17:57:52 -0300

    primeiro projeto

---
+++ PrimeiroProjeto/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Welcome to the first project. I hope we can learn to code java together!");
+    }
+}
